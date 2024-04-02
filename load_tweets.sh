e55a5d419aae23225122b8330d7e14b652630c66
rachelHoman <rachelmarie001@aol.com>
2024-04-02 11:53:34 -0700

    altering load

--- load_tweets.sh
+++ load_tweets.sh
@@ -16,5 +16,5 @@ done
 echo 'load denormalized'
 for file in $files; do
     # use SQL's COPY command to load data into pg_denormalized
-    unzip -p "$file" | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:2363/ -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
+    time unzip -p "$file" | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:2363 -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
 done
